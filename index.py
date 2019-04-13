74f37ea0cd39b85d120304ddd3f1c80b2ddea55f
dejanbasic <dejan555@gmail.com>
2019-04-13 19:23:43 +0200

    initial commit

---
+++ index.py
@@ -0,0 +1,10 @@
+user_mood = input("In what mood are you today: ")
+
+if user_mood == "happy":
+    print("It is great to see you happy!")
+elif user_mood == "nervous":
+    print("Take a deep breath 3 times.")
+elif user_mood == "sad":
+    print("Cheer up, mate!")
+else:
+    print("Life is too short, enjoy it! :)")
